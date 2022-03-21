9a06855754510117244b9728422363fa15b806a7
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2022-03-21 10:28:46 +0100

    quiz

---
+++ quizs.php
@@ -0,0 +1,54 @@
+<?php
+$quizs = [
+    0 => [
+        "Title" => "Connaissances générales",
+        "Picture" => "",
+        "Author" => "Cédric",
+        "Questions" => [
+            0 => "Jusqu’en 1923, comment s’appelait la ville turque d’Istanbul ?",
+            1 => "Combien de rayures y a-t-il sur le drapeau américain ?",
+            2 => "En combien de jours la Terre tourne-t-elle autour du Soleil ?",
+        ]
+    ],
+    1 => [
+        "Title" => "Géographie du monde",
+        "Picture" => "",
+        "Author" => "Cédric",
+        "Questions" => [
+            0 => "Quelle est la capitale du Canada ?",
+            1 => "Quel pays possède le plus grand nombre d’îles dans le monde ?",
+        ]
+    ],
+    2 => [
+        "Title" => "Langue, littérature et art",
+        "Picture" => "",
+        "Author" => "Cédric",
+        "Questions" => [
+            0 => "Quel artiste a peint le plafond de la chapelle Sixtine à Rome ? ",
+            1 => "Quel graffeur célèbre est originaire de Bristol ?",
+            2 => "Quelle langue comprend le plus grand nombre de mots (selon les entrées du dictionnaire) ?",
+        ]
+    ],
+    3 => [
+        "Title" => "Sport",
+        "Picture" => "",
+        "Author" => "Cédric",
+        "Questions" => [
+            0 => "Quel pilote a-t-il remporté le plus de championnats de Formule 1 ?",
+            1 => "Comment la société de vêtements Nike s’appelait-elle à l’origine ?",
+            2 => "Dans quelle ville les premiers Jeux olympiques modernes se sont-ils tenus ? ",
+            3 => "Quelle équipe de football(club) est-elle connue sous le nom des « Diables rouges » ?"
+        ]
+    ],
+    4 => [
+        "Title" => "Cinéma et télévision",
+        "Picture" => "",
+        "Author" => "Cédric",
+        "Questions" => [
+            0 => "En quelle année Netflix a-t-il été créé?",
+            1 => "Quelle a été la série la plus regardée sur Netflix en 2019 ?",
+            2 => "Quel est le nom du café dans la sitcom Friends ? ",
+            3 => "Citez le nom du premier film de Disney ?"
+        ]
+    ],
+];
